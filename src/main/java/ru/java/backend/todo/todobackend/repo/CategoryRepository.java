e6cb4962988130269e2ef99a3b825227fc97b74a
Pavel Semenov <PNSemenov@sberbank.ru>
2021-12-29 08:47:01 +0400

    added Priority controller, repository, service

--- src/main/java/ru/java/backend/todo/todobackend/repo/CategoryRepository.java
+++ src/main/java/ru/java/backend/todo/todobackend/repo/CategoryRepository.java
@@ -21,9 +21,7 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
             "(:title is null or :title='' " + // если передадим параметр title пустым, то выберутся все записи (сработает именно это условие)
             " or lower(c.title) like lower(concat('%', :title,'%'))) " + // если параметр title не пустой, то выполнится уже это условие
             " and c.user.email=:email  " + // фильтрация для конкретного пользователя
-            " order by c.title asc") // сортировка по названию
+            " order by c.title asc")
+    // сортировка по названию
     List<Category> findByTitle(@Param("title") String title, @Param("email") String email);
-
-
-
 }
