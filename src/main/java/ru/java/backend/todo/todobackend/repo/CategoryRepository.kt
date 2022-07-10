21be41e39d8c46b99d4b4fc09d7a61a6458c84ed
Pavel Semenov <PNSemenov@sberbank.ru>
2022-07-10 16:51:15 +0300

    convertated repository kotlin

--- src/main/java/ru/java/backend/todo/todobackend/repo/CategoryRepository.kt
+++ src/main/java/ru/java/backend/todo/todobackend/repo/CategoryRepository.kt
@@ -1,27 +1,25 @@
-package ru.java.backend.todo.todobackend.repo;
+package ru.java.backend.todo.todobackend.repo
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import ru.java.backend.todo.todobackend.entity.Category;
-
-import java.util.List;
-
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+import ru.java.backend.todo.todobackend.entity.Category
 
 //ооп: абстракция - реализация - здесь описываем все доступные доступы к данным
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-
+interface CategoryRepository : JpaRepository<Category, Long> {
     // поиск категорий пользователя (по названию)
-    List<Category> findByUserEmailOrderByTitleAsc(String email);
+    fun findByUserEmailOrderByTitleAsc(email: String): List<Category>
 
     // поиск значений по названию для конкретного пользователя
-    @Query("SELECT c FROM Category c where " +
-            "(:title is null or :title='' " + // если передадим параметр title пустым, то выберутся все записи (сработает именно это условие)
-            " or lower(c.title) like lower(concat('%', :title,'%'))) " + // если параметр title не пустой, то выполнится уже это условие
-            " and c.user.email=:email  " + // фильтрация для конкретного пользователя
-            " order by c.title asc")
-    // сортировка по названию
-    List<Category> findByTitle(@Param("title") String title, @Param("email") String email);
+    @Query(
+        "SELECT c FROM Category c where " +
+                "(:title is null or :title='' " +  // если передадим параметр title пустым, то выберутся все записи (сработает именно это условие)
+                " or lower(c.title) like lower(concat('%', :title,'%'))) " +  // если параметр title не пустой, то выполнится уже это условие
+                " and c.user.email=:email  " +  // фильтрация для конкретного пользователя
+                " order by c.title asc"
+    )
+    fun  // сортировка по названию
+            findByTitle(@Param("title") title: String?, @Param("email") email: String): List<Category>
 }
--- src/main/java/ru/java/backend/todo/todobackend/repo/StatRepository.kt
+++ src/main/java/ru/java/backend/todo/todobackend/repo/StatRepository.kt
@@ -1,11 +1,10 @@
-package ru.java.backend.todo.todobackend.repo;
+package ru.java.backend.todo.todobackend.repo
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.java.backend.todo.todobackend.entity.Stat;
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import ru.java.backend.todo.todobackend.entity.Stat
 
 @Repository
-public interface StatRepository extends CrudRepository<Stat, Long> {
-
-    Stat findByUserEmail(String email); //связь один к одному
+interface StatRepository : CrudRepository<Stat, Long> {
+    fun findByUserEmail(email: String): Stat //связь один к одному
 }
